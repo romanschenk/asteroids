6819f5ca3977c0eaf8f057ed2da02dcc69d95078
romanschenk <romanschenk@hotmail.com>
2024-10-17 22:30:44 +0200

    added groups to streamline update and draw calls

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ from constants import *
 from player import Player
 
 def main():
-    print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
+        screen.fill(color="black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
-        screen.fill(color="black")
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
